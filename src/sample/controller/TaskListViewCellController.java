ac299b7f2d89c4a459c892e7ac11da31713c8c1e
YourAss <yrik.murashko@gmail.com>
2021-04-25 12:09:42 +0300

    Custom ListView (ListView<String> return to ListView<Task>)

--- src/sample/controller/TaskListViewCellController.java
+++ src/sample/controller/TaskListViewCellController.java
@@ -3,14 +3,27 @@ package sample.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import sample.model.Task;
 
+import java.awt.*;
 import java.io.IOException;
 
 public class TaskListViewCellController extends ListCell<Task> {
     @FXML
-    Label priorityLabel, taskLabel;
+    Label labelTask, labelPriority;
+    @FXML
+    CheckBox checkBoxDone;
+
+    @FXML
+    ImageView priorityImage;
 
     @FXML
     HBox hBox;
@@ -34,8 +47,8 @@ public class TaskListViewCellController extends ListCell<Task> {
             setText(null);
             setGraphic(null);
         } else {
-            priorityLabel.setText(task.getPriority());
-            taskLabel.setText(task.getTask());
+            labelPriority.setText(task.getPriority());
+            labelTask.setText(task.getTask());
 
             this.setTooltip(new Tooltip(task.toString()));
 
